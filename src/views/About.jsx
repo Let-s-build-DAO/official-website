376e0aa0e21bc13cbbd080e63fa541eb819c8bbc
dprincecoder <dprincecoder@gmail.com>
2022-10-17 18:45:58 +0100

    created the Sponsors section

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/white_paper_page/header/Header';
 
+import Sponsors from '../components/white_paper_page/sponsors/Sponsors';
 function About(props) {
   return (
     <div>
       
+      <Sponsors />
     </div>
   );
 }
