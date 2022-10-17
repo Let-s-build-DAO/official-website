f507819a67ddc90af2f18997da92ac9c5d51666a
dprincecoder <dprincecoder@gmail.com>
2022-10-17 17:16:02 +0100

    added hero section, plus responsivenes

--- src/components/white_paper_page/header/Header.jsx
+++ src/components/white_paper_page/header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Nav from "./nav/Nav";
+import Hero from "./hero/Hero";
 
 const Header = () => {
   return (
-    <Nav />
+    <div
+      className={`nav h-[80vh] w-full`}
+      style={{ backgroundImage: `url("white_paper_assets/vector-bg.svg")` }}
+    >
+      <Nav />
+      <Hero />
+    </div>
   );
 };
 
