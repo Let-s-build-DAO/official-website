b27f250eb681ca820a1badfd966d187e33757ff8
Dope-Genius <samueladeyemi244@gmail.com>
2022-10-21 19:49:05 +0100

    created the sponsor contact and newsletter section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+  
+    extend: {
+      colors : {
+        "main" : "#E98BC7"
+      },
+    },
   },
   plugins: [],
 }
